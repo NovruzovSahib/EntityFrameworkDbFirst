b82796ff3ef6ad0eba5d701328ca25735ac28574
Sahib Novruzov <sahib.novruzov33@hotmail.com>
2023-08-17 16:25:37 +0400

    Add project files.

---
+++ EntityFrameworkDbFirst/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace EntityFrameworkDbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.insertbtn = new System.Windows.Forms.Button();
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.inserttxt = new System.Windows.Forms.TextBox();
+            this.searchtxt = new System.Windows.Forms.TextBox();
+            this.updateidtxt = new System.Windows.Forms.TextBox();
+            this.updatetxt = new System.Windows.Forms.TextBox();
+            this.deleteidtxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectbtn
+            // 
+            this.selectbtn.BackColor = System.Drawing.Color.Transparent;
+            this.selectbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectbtn.Location = new System.Drawing.Point(12, 13);
+            this.selectbtn.Name = "selectbtn";
+            this.selectbtn.Size = new System.Drawing.Size(473, 72);
+            this.selectbtn.TabIndex = 0;
+            this.selectbtn.Text = "SELECT";
+            this.selectbtn.UseVisualStyleBackColor = false;
+            this.selectbtn.Click += new System.EventHandler(this.selectbtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(491, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(715, 332);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // insertbtn
+            // 
+            this.insertbtn.BackColor = System.Drawing.Color.Transparent;
+            this.insertbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertbtn.Location = new System.Drawing.Point(12, 147);
+            this.insertbtn.Name = "insertbtn";
+            this.insertbtn.Size = new System.Drawing.Size(250, 40);
+            this.insertbtn.TabIndex = 2;
+            this.insertbtn.Text = "INSERT";
+            this.insertbtn.UseVisualStyleBackColor = false;
+            this.insertbtn.Click += new System.EventHandler(this.insertbtn_Click);
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchbtn.Location = new System.Drawing.Point(12, 91);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(250, 42);
+            this.searchbtn.TabIndex = 3;
+            this.searchbtn.Text = "SEARCH";
+            this.searchbtn.UseVisualStyleBackColor = true;
+            this.searchbtn.Click += new System.EventHandler(this.searchbtn_Click);
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatebtn.Location = new System.Drawing.Point(12, 193);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(250, 85);
+            this.updatebtn.TabIndex = 4;
+            this.updatebtn.Text = "UPDATE";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            this.updatebtn.Click += new System.EventHandler(this.updatebtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deletebtn.Location = new System.Drawing.Point(12, 285);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(250, 40);
+            this.deletebtn.TabIndex = 5;
+            this.deletebtn.Text = "DELETE";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click_1);
+            // 
+            // inserttxt
+            // 
+            this.inserttxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inserttxt.Location = new System.Drawing.Point(269, 147);
+            this.inserttxt.Name = "inserttxt";
+            this.inserttxt.Size = new System.Drawing.Size(216, 40);
+            this.inserttxt.TabIndex = 6;
+            // 
+            // searchtxt
+            // 
+            this.searchtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchtxt.Location = new System.Drawing.Point(321, 91);
+            this.searchtxt.Name = "searchtxt";
+            this.searchtxt.Size = new System.Drawing.Size(48, 40);
+            this.searchtxt.TabIndex = 7;
+            // 
+            // updateidtxt
+            // 
+            this.updateidtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateidtxt.Location = new System.Drawing.Point(321, 189);
+            this.updateidtxt.Name = "updateidtxt";
+            this.updateidtxt.Size = new System.Drawing.Size(48, 40);
+            this.updateidtxt.TabIndex = 8;
+            // 
+            // updatetxt
+            // 
+            this.updatetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatetxt.Location = new System.Drawing.Point(269, 235);
+            this.updatetxt.Name = "updatetxt";
+            this.updatetxt.Size = new System.Drawing.Size(216, 40);
+            this.updatetxt.TabIndex = 9;
+            // 
+            // deleteidtxt
+            // 
+            this.deleteidtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteidtxt.Location = new System.Drawing.Point(321, 281);
+            this.deleteidtxt.Name = "deleteidtxt";
+            this.deleteidtxt.Size = new System.Drawing.Size(48, 40);
+            this.deleteidtxt.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 42);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(269, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 42);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(269, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 42);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 332);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteidtxt);
+            this.Controls.Add(this.updatetxt);
+            this.Controls.Add(this.updateidtxt);
+            this.Controls.Add(this.searchtxt);
+            this.Controls.Add(this.inserttxt);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.updatebtn);
+            this.Controls.Add(this.searchbtn);
+            this.Controls.Add(this.insertbtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.selectbtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EntityForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button insertbtn;
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.TextBox inserttxt;
+        private System.Windows.Forms.TextBox searchtxt;
+        private System.Windows.Forms.TextBox updateidtxt;
+        private System.Windows.Forms.TextBox updatetxt;
+        private System.Windows.Forms.TextBox deleteidtxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EntityFrameworkDbFirst/Form1.cs
@@ -0,0 +1,129 @@
+﻿using EntityFrameworkDbFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDbFirst
+{
+    public partial class Form1 : Form
+    {
+        private MYDATABASEEntities dbcontext;
+        public Form1()
+        {
+            InitializeComponent();
+            dbcontext = new MYDATABASEEntities();
+        }
+
+        private void selectbtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbcontext.COUNTRies.Select(s => new { s.ID, s.COUNTRYNAME }).ToList();
+        }
+
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string countryName = inserttxt.Text.Trim();
+
+                if (string.IsNullOrEmpty(countryName))
+                {
+                    MessageBox.Show("Please Add Country Name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                COUNTRY country = new COUNTRY()
+                {
+                    COUNTRYNAME = inserttxt.Text.Trim(),
+                };
+                dbcontext.COUNTRies.Add(country);
+                dbcontext.SaveChanges();
+                MessageBox.Show("Country inserted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error inserting country: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void searchbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int countryid = int.Parse(searchtxt.Text.Trim());
+
+                var searchresult = dbcontext.COUNTRies.FirstOrDefault(s => s.ID == countryid);
+
+                if (searchresult != null)
+                {
+                    dataGridView1.DataSource = new List<COUNTRY> { searchresult };
+                }
+                else
+                {
+                    MessageBox.Show("Country not found.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Please add Id to search {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int countryid = int.Parse(updateidtxt.Text.Trim());
+                var updateresult = dbcontext.COUNTRies.FirstOrDefault(s => s.ID == countryid);
+
+                if (updateresult!=null)
+                {
+                    updateresult.COUNTRYNAME = updatetxt.Text.Trim();
+                    dbcontext.SaveChanges();
+                    MessageBox.Show("Country updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+               
+                  else
+                {
+                    MessageBox.Show("Country not found.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating country: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void deletebtn_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                int countryid = int.Parse(deleteidtxt.Text.Trim());
+                var deleteresult = dbcontext.COUNTRies.FirstOrDefault(s => s.ID == countryid);
+
+                if (deleteresult != null)
+                {
+                    dbcontext.COUNTRies.Remove(deleteresult);
+                    dbcontext.SaveChanges();
+                    MessageBox.Show("Country deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Country not found.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting country: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
